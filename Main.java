a09419fb25996ad0f612ca5c8a39c9ce69e4c75c
Matheusprower <96206325+Matheusprower@users.noreply.github.com>
2025-06-30 22:08:26 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+import DAO.ConexaoA3;
+import VIEW.frLoginVIEW;
+import javax.swing.*;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+                frLoginVIEW objfrloginview = new frLoginVIEW();
+                objfrloginview.setVisible(true);
+                objfrloginview.setTitle("Login");
+    }
+}
